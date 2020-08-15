637c44266b610316a2bfc47a6fb215fb10dcb64c
patrickmclean@yahoo.com <patrickmclean@yahoo.com>
2020-08-15 16:49:19 +0200

    x

--- convert.sh
+++ convert.sh
@@ -1,6 +1,4 @@
-bash stylize_image.sh ./image_input/Alison.jpg ./styles/rembrandthill.jpg ./results/a2.jpg
-bash stylize_image.sh ./image_input/Alison.jpg ./styles/rembrandtpaint.jpg ./results/a3.jpg
-bash stylize_image.sh ./image_input/IMG_1791.jpg ./styles/rembrandtpaint.jpg ./results/ch1.jpg
+bash stylize_image.sh ./image_input/Catja.jpg ./styles/klum.jpg ./results/c3.jpg
 
 
 

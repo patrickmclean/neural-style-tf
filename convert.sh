e08b27eef9b1ea337709020643cf6fdcb68baebe
patrickmclean@yahoo.com <patrickmclean@yahoo.com>
2020-08-15 13:16:26 +0200

    add execute script

---
+++ convert.sh
@@ -0,0 +1,5 @@
+bash stylize_image.sh ./image_input/IMG_1755.jpg ./styles/rembrandthill.jpg
+bash stylize_image.sh ./image_input/IMG_1677.jpg ./styles/rembrandtcountry.jpg
+bash stylize_image.sh ./image_input/IMG_0221.jpg ./styles/delftetching.jpg
+bash stylize_image.sh ./image_input/IMG_1674.jpg ./styles/rembrandtportrait.jpg
+bash stylize_image.sh ./image_input/IMG_7414.jpg ./styles/blankpeople.jpg

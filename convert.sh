7d2178220c542254040bd443e646e5df65a3d6eb
patrickmclean@yahoo.com <patrickmclean@yahoo.com>
2020-08-16 21:43:03 +0200

    x

--- convert.sh
+++ convert.sh
@@ -1,14 +1,9 @@
-bash stylize_image.sh ./image_input/IMG_1585.jpg ./styles/monetparis.jpg
 bash stylize_image.sh ./image_input/IMG_1585.jpg ./styles/monetparis2.jpg
-bash stylize_image.sh ./image_input/IMG_1585.jpg ./styles/monetparis3.jpg
 
-bash stylize_image.sh ./image_input/IMG_1589.jpg ./styles/monetparis.jpg
 bash stylize_image.sh ./image_input/IMG_1589.jpg ./styles/monetparis2.jpg
-bash stylize_image.sh ./image_input/IMG_1589.jpg ./styles/monetparis3.jpg
 
-bash stylize_image.sh ./image_input/IMG_1609.jpg ./styles/monetparis.jpg
 bash stylize_image.sh ./image_input/IMG_1609.jpg ./styles/monetparis2.jpg
-bash stylize_image.sh ./image_input/IMG_1609.jpg ./styles/monetparis3.jpg
+
 
 
 
